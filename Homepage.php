cc6746dbdb83198d143caca2837c51ac1653552b
Harley0211 <harleydavevalencia@gmail.com>
2025-03-28 16:55:00 +0800

    updated 4:54 3/28/2025

--- Homepage.php
+++ Homepage.php
@@ -71,13 +71,24 @@
         </div>
 
         <div class="shape-container">
-            <div class="text-container">
-                <h1 class="welcome-text">WELCOME</h1>
-                <p class="scan-text">Please scan your ID here</p>
-                <div class="scan-icon"><svg width="100" height="100" fill="#1A1A1A" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <div class="primary-container">
+                <button class="guest-button">
+                    <svg width="100" height="100" fill="#0866ff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 6a3.75 3.75 0 1 0 0 7.5A3.75 3.75 0 0 0 12 6Zm0 6a2.25 2.25 0 1 1 0-4.5 2.25 2.25 0 0 1 0 4.5Z"></path>
+                        <path d="M12 1.5A10.5 10.5 0 1 0 22.5 12 10.512 10.512 0 0 0 12 1.5ZM7.5 19.782V18.75a2.253 2.253 0 0 1 2.25-2.25h4.5a2.253 2.253 0 0 1 2.25 2.25v1.032a8.924 8.924 0 0 1-9 0Zm10.494-1.088A3.751 3.751 0 0 0 14.25 15h-4.5a3.751 3.751 0 0 0-3.744 3.694 9 9 0 1 1 11.988 0Z"></path>
+                    </svg>
+                    Guest
+                </button>
+                <div class="text-container">
+                    <h1 class="welcome-text">WELCOME</h1>
+                    <p class="scan-text">Please scan your ID here</p>
+                </div>
+                <div class="scan-icon">
+                    <svg width="100" height="100" fill="#1A1A1A" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path d="M5 7h2v10H5V7Zm9 0h1v10h-1V7Zm-4 0h3v10h-3V7ZM8 7h1v10H8V7Zm8 0h3v10h-3V7Z"></path>
                         <path d="M4 5h4V3H4c-1.103 0-2 .897-2 2v4h2V5Zm0 16h4v-2H4v-4H2v4c0 1.103.897 2 2 2ZM20 3h-4v2h4v4h2V5c0-1.103-.897-2-2-2Zm0 16h-4v2h4c1.103 0 2-.897 2-2v-4h-2v4Z"></path>
-                    </svg></div>
+                    </svg>
+                </div>
             </div>
         </div>
     </div>
